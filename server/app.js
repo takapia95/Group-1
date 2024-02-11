a89d4ce00df9144f3e6d4334abb09810d8decc6f
Maria Mills <cxwinterx@gmail.com>
2024-02-11 18:18:41 -0500

    feat: Add outline of backend API endpoints

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
 const cors = require('cors'); // Cross-Origin Resource Sharing
 const axios = require('axios');
-require('dotenv').config();
+const express = require('express');
+const app = express();
 const port = 3001;
+require('dotenv').config();
 
+// Middleware:
 // Enable CORS for all requests bc we're requesting from a different origin
 app.use(cors());
-
+// Enable parsing of JSON data
 app.use(express.json());
 
 // Mock authenticated - this would be a db
@@ -15,14 +16,20 @@ const users = {
     'maria': { name: 'maria' } // User: maria
 };
 
+// TODO: display frontend? or just remove this route
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Login
+// @desc: Login
+// @route: POST /login
+// @access: Public
 app.post('/login', (req, res) => {
     const { username } = req.body;
     const user = users[username];
+    if (!user) {
+        res.status(401).json({ message: 'User not found' });
+    }
     if (user) {
         res.json({
             loggedIn: true,
@@ -33,7 +40,10 @@ app.post('/login', (req, res) => {
     }
 });
 
-// Search - TripAdvisor API
+
+// @desc: Search for locations using the TripAdvisor API
+// @route: GET /search
+// @access: Private
 app.get('/search', async (req, res) => {
     const { searchQuery } = req.query;
     const url = `https://api.content.tripadvisor.com/api/v1/location/search?key=${process.env.TRIPADVISOR_API_KEY}&searchQuery=${searchQuery}&language=en`;
@@ -45,6 +55,34 @@ app.get('/search', async (req, res) => {
         res.status(500).json({ message: "Error fetching data", error: error.response });
     }
 });
+
+// @desc: Get user journals
+// @route: GET /journals
+// @access: Private
+app.get('/journals', (req, res) => {
+    res.send('Get user journals');
+})
+
+// @desc: Add a new journal
+// @route: POST /journals
+// @access: Private
+app.post('/journals', (req, res) => {
+    res.send('Add a new journal');
+})
+
+// @desc: Update a journal
+// @route: PUT /journals/:id
+// @access: Private
+app.put('/journals/:id', (req, res) => {
+    res.send('Update a journal');
+})
+
+// @desc: Delete a journal
+// @route: DELETE /journals/:id
+// @access: Private
+app.delete('/journals/:id', (req, res) => {
+    res.send('Delete a journal');
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
