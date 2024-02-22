5982ef6d840ddce1a3e19ed03e5f80d441314bb7
Maria Mills <cxwinterx@gmail.com>
2024-02-22 17:05:50 -0500

    feat: Add Home component

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Showcase from "./Showcase";
+
+const Home = () => {
+    return (
+        <main>
+            <Hero />
+            <Showcase />
+        </main>
+    );
+}
+export default Home;
