bd4602b917c394a3f9f72ef0dbe03c5a0766f0b2
Maria Mills <cxwinterx@gmail.com>
2024-02-25 20:30:41 -0500

    refactor: Minor enhancements to Search.jsx & SearchResults.jsx
    - Moved nodemon to devDependencies.
    - Added validation for empty search queries with alert, implemented `maxLength` and `required` attributes for input, and changed ternary operations to logical OR (||).
    - Converted filter links (<a>) to buttons (<button>) with POC onClick handlers.

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -30,17 +30,19 @@ const SearchResults = () => {
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 text-center max-w-2xl pb-8 mx-auto sm:text-5xl">Search Results</h1>
             <div className="flex justify-center gap-4 flex-wrap">
                 {filters.map((filter) => (
-                    <a href="/#"
-                       className="rounded-md bg-amber-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500">
+                    <button
+                        onClick={() => console.log('Filter:', filter.name)}
+                        key={filter.name}
+                        className="rounded-md bg-amber-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500">
                         {/*<i className="">{filter.icon}</i>*/}
                         {filter.name}
-                    </a>
+                    </button>
                 ))}
             </div>
             {searchResults.length > 0 ? (
                 <div className="flex flex-wrap gap-3.5 justify-center py-8">
                     {searchResults.map((result, index) => (
-                        <div key={index}
+                        <div key={result.location_id}
                              className="bg-amber-100 p-10 hover:scale-110 text-gray-800 max-w-lg transition ease-in-out hover:cursor-pointer rounded-md">
                             <h4 className="text-xl font-bold tracking-tight border-b border-amber-500 mb-3">{result.name}</h4>
                         <p className="max-w-52 tracking-tight">{result.address_obj?.address_string}</p>
