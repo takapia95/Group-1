99301a6e42bffc2975ef2fe75bbc695e9a66ff16
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:47:32 -0400

    Update Showcase.test.jsx

--- client/src/tests/Showcase.test.jsx
+++ client/src/tests/Showcase.test.jsx
@@ -5,12 +5,10 @@ import Showcase from '../components/Showcase';
 describe('Showcase Component', () => {
     it('renders correctly', () => {
         const { getByText, getAllByAltText } = render(<Showcase />);
-        
-        // Check for the presence of text content
+
         expect(getByText("Add photos and maps")).toBeInTheDocument();
         expect(getByText("Share your Voyage!")).toBeInTheDocument();
 
-        // Check for the presence of images
         const images = getAllByAltText("Product screenshot");
         expect(images.length).toBe(2);
     });
