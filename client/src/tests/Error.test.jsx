358985f9cb05c38545198429ea260624795cfb6f
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:39:00 -0400

    Update Error.test.jsx

--- client/src/tests/Error.test.jsx
+++ client/src/tests/Error.test.jsx
@@ -5,16 +5,13 @@ import Error from '../components/Error';
 test('renders Error component', () => {
   render(<Error />);
   
-  // Check if the logo is rendered
   const logoElement = screen.getByAltText('Voyage');
   expect(logoElement).toBeInTheDocument();
 
-  // Check if the error message is rendered
   const errorMessageElement = screen.getByText(/This page does not exist/i);
   expect(errorMessageElement).toBeInTheDocument();
 
-  // Check if the back to home link is rendered
   const backToHomeLink = screen.getByText(/Back to home/i);
   expect(backToHomeLink).toBeInTheDocument();
-  expect(backToHomeLink.getAttribute('href')).toBe('/#'); // Ensure the link has the correct href attribute
+  expect(backToHomeLink.getAttribute('href')).toBe('/#'); 
 });
