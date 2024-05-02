b6ba64d257045a1252d9d8111fd913ba5ce1f898
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:39:36 -0400

    Update Footer.test.jsx

--- client/src/tests/Footer.test.jsx
+++ client/src/tests/Footer.test.jsx
@@ -6,13 +6,11 @@ describe('Footer Component', () => {
     it('renders all navigation links', () => {
         render(<Footer />);
 
-        // Find all links
         const instagramLink = screen.getByText('Instagram');
         const xLink = screen.getByText('X');
         const githubLink = screen.getByText('GitHub');
         const youtubeLink = screen.getByText('YouTube');
 
-        // make sure all links are rendered
         expect(instagramLink).toBeInTheDocument();
         expect(xLink).toBeInTheDocument();
         expect(githubLink).toBeInTheDocument();
