a9640b9ee3b54a22c32ac5b27a693fc379808bff
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 09:02:11 -0400

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,18 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-// auth middleware - checks if the user is authenticated using a jwt token
 const authenticateToken = (req, res, next) => {
-    console.log(req.headers.authorization); // debug
+    console.log(req.headers.authorization); 
 
-    // check if the request has an authorization header and if it starts with 'Bearer '
     const authHeader = req.headers.authorization;
     if (authHeader?.startsWith('Bearer ')) {
         const token = authHeader.split(' ')[1];
         try {
-            // verify the token using the secret key
             const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-            req.user = verified; // Add user info to request
-            next(); // Proceed to the next middleware or route handler, in this case the route handler bc no other middleware
+            req.user = verified; 
+            next(); 
         } catch (error) {
             console.error('JWT verification error:', error);
             res.status(403).json({ message: 'Invalid token' });
