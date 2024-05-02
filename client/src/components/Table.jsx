c714f8c0179bf93397955c2876a405a68d84eaaa
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:32:42 -0400

    Update Table.jsx

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -15,7 +15,6 @@ const photos = {
     travelPhoto4: travelPhoto4,
 };
 
-// Define icons for the public and private states
 const EyeIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
