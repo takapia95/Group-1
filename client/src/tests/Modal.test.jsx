43515790c17669adabe393211204b3e33cfd2848
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:43:44 -0400

    Update Modal.test.jsx

--- client/src/tests/Modal.test.jsx
+++ client/src/tests/Modal.test.jsx
@@ -4,7 +4,6 @@ import Modal from '../components/Modal';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useStore } from '../resources/store';
 
-// mock the Zustand store
 jest.mock('../resources/store', () => ({
     useStore: jest.fn(),
 }));
@@ -13,28 +12,24 @@ describe('Modal Component', () => {
     const mockOnClose = jest.fn();
 
     beforeEach(() => {
-        // reset mock function's state
         mockOnClose.mockReset();
     });
 
     it('renders Login content by default when open', () => {
         useStore.mockImplementation(() => ({ modalContent: '' }));
         render(<Modal isOpen={true} onClose={mockOnClose} />);
-        // Check that the login button is rendered
         expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
     });
 
     it('renders Login content when set to login and open', () => {
         useStore.mockImplementation(() => ({ modalContent: 'login' }));
         render(<Modal isOpen={true} onClose={mockOnClose} />);
-        // Check that the login button is rendered
         expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
     });
 
     it('renders Registration content when set to register and open', () => {
         useStore.mockImplementation(() => ({ modalContent: 'register' }));
         render(<Modal isOpen={true} onClose={mockOnClose} />);
-        // Check that the register button is rendered
         expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
     });
 
