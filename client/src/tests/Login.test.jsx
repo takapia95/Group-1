23799a6fc63b3a416247435a9265f37d6ad6c009
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:43:08 -0400

    Update Login.test.jsx

--- client/src/tests/Login.test.jsx
+++ client/src/tests/Login.test.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Login from '../components/Login';
 import { render, screen } from '@testing-library/react';
 
-// mock the axios
 jest.mock('axios');
 
-// mock the Zustand store
 jest.mock('../resources/store', () => ({
     useStore: jest.fn(),
 }));
@@ -18,7 +16,4 @@ describe('Login component', () => {
         expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
         expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
     });
-
-    //TODO: test switch to Register component when the register button is clicked
-    // TODO: test login submission
 });
