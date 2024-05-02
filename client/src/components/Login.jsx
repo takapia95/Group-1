927e841df962d8847c0c734fa53bf0c824f82299
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:29:38 -0400

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -60,7 +60,6 @@ const Login = () => {
                 >
                     Login
                 </button>
-                {/*  if there's an error  */}
                 {loginError && (
                     <p className="text-red-500 text-sm mt-2">{loginError}</p>
                 )}
