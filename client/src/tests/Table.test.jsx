d2da2f6f916ffe9a6b7ce2cd6936d140bb0badf0
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:48:39 -0400

    Update Table.test.jsx

--- client/src/tests/Table.test.jsx
+++ client/src/tests/Table.test.jsx
@@ -3,13 +3,11 @@ import Table from '../components/Table';
 import { useStore } from '../resources/store';
 import { useNavigate } from 'react-router-dom';
 
-// Mock the useNavigate hook
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: jest.fn(),
 }));
 
-// Mock the useStore hook
 jest.mock('../resources/store', () => ({
   useStore: jest.fn(),
 }));
@@ -19,7 +17,6 @@ describe('Table component', () => {
   window.confirm = mockConfirm;
 
   beforeEach(() => {
-    // Reset mockConfirm before each test
     mockConfirm.mockClear();
   });
 
@@ -35,10 +32,8 @@ describe('Table component', () => {
             mockConfirm.mockReturnValue(true);
 
 
-            // Click the delete button
             fireEvent.click(getByText('Delete'));
 
-            // Check if deleteJournalEntry is called with the correct argument
             expect(deleteJournalEntry).toHaveBeenCalledWith('1');
     });
 
@@ -51,10 +46,8 @@ describe('Table component', () => {
 
         const { container } = render(<Table entries={entries} />);
 
-        // Click the edit button with the unique class name
         fireEvent.click(container.querySelector('.text-amber-500'));
 
-        // Check if navigate is called with the correct route
         expect(navigate).toHaveBeenCalledWith('/edit-entry/1');
     });
 });
