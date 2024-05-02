db74fbdaa326743a04d08615e0f7edcd485f0f42
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2024-05-02 08:40:33 -0400

    Update Form.test.jsx

--- client/src/tests/Form.test.jsx
+++ client/src/tests/Form.test.jsx
@@ -4,10 +4,8 @@ import {act, render, screen} from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// mock the axios
 jest.mock('axios');
 
-// mock the Zustand store
 jest.mock('../resources/store', () => ({
     useStore: () => ({
         addJournalEntry: jest.fn(),
@@ -23,14 +21,12 @@ describe('Form Component', () => {
             </Router>
         );
 
-        // Find all form fields
         const titleField = screen.getByLabelText(/Title/i);
         const aboutField = screen.getByLabelText(/About/i);
         const coverPhoto = screen.getByText("Cover photo");
         const yesRadio = screen.getByLabelText(/Yes/i);
         const noRadio = screen.getByLabelText(/No/i);
 
-        // make sure all form fields are rendered
         expect(titleField).toBeInTheDocument();
         expect(aboutField).toBeInTheDocument();
         expect(coverPhoto).toBeInTheDocument();
@@ -55,7 +51,6 @@ describe('Form Component', () => {
             </Router>
         );
 
-        // wrap the interactions in act because we are using async/await
         await act(async () => {
             await userEvent.type(screen.getByLabelText(/Title/i), 'My Title');
             await userEvent.type(screen.getByLabelText(/About/i), 'My About');
@@ -63,7 +58,5 @@ describe('Form Component', () => {
             const submitButton = screen.getByRole('button', { name: /save/i });
             userEvent.click(submitButton);
         });
-
-        // TODO: check if the form data is sent to the server
     });
 });
