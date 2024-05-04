d1e3a33d1185d8f41129c6b5a5802bc914e02748
Maria Mills <cxwinterx@gmail.com>
2024-05-04 10:40:12 -0400

    Fix cypress journal tests

--- client/cypress/e2e/journals.cy.js
+++ client/cypress/e2e/journals.cy.js
@@ -9,7 +9,7 @@ describe('Logged in tests', () => {
 
     it('add new journal takes to search', () => {
         cy.get('a').contains('View Journals').click()
-        cy.get('input').should('have.length', 1);
+        cy.get('input').should('have.length', 0);
     });
 
     it('searching with query should navigate to results page', () => {
@@ -18,12 +18,9 @@ describe('Logged in tests', () => {
     });
 
     it('add new journal starts new journal', () => {
-        cy.get('#search').type('test{enter}');
-        cy.url().should('include', '/results?searchQuery=test');
-        cy.get('h4').contains('Test Track').click();
-        cy.url().should('include', '/location/');
-        cy.get('button').contains('Add Journal Entry').click()
-        cy.url().should('include', '/add-entry');
+        cy.get('a').contains('View Journals').click()
+        cy.get('button').contains('Start Journaling').click()
+        cy.get('input').should('have.length', 1);
     });
 
     it('add new journal entry', () => {
@@ -40,23 +37,21 @@ describe('Logged in tests', () => {
         cy.get('table').should('have.length', 1);
     });
 
-
-    it('delete journal entry', () => {
-        cy.get('#search').type('test{enter}');
-        cy.url().should('include', '/results?searchQuery=test');
-        cy.get('h4').contains('Test Track').click();
-        cy.url().should('include', '/location/');
-        cy.get('button').contains('Add Journal Entry').click()
-        cy.url().should('include', '/add-entry');
-
-        cy.get('#title').type('Test Title');
-        cy.get('#about').type('Test Content');
+    it('should edit journal entry', () => {
+        cy.get('a').contains('View Journals').click()
+        cy.get('button').contains('Edit').click();
+        cy.get('#title').type(' Edited');
+        cy.get('#about').type(' Edited');
+        cy.get('#no').check();
         cy.get('button').contains('Save').click();
         cy.get('table').should('have.length', 1);
+    });
 
+
+    it('delete journal entry', () => {
+        cy.get('a').contains('View Journals').click()
         cy.get('button').contains('Delete').click();
         cy.get('table').should('have.length', 0);
     });
-
 
 });
